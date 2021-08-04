90abc2e7f82fe76fb7b99d66aa381961f3e644e0
RKKING1413 <81173678+RKKING1413@users.noreply.github.com>
2021-08-05 00:05:03 +0530

    Create q5.js

---
+++ q5.js
@@ -0,0 +1,13 @@
+// Q.5 > What will be the output for the code?
+
+
+var arr = [2, 56, 78, 34, 65];
+var new_arr = arr.map(function (num) {
+    return num / 2;
+});
+console.log(new_arr);
+
+
+/* output :
+Array(5) [ 1, 28, 39, 17, 32.5 ]    // all the numbers are devided by 2 
+*/
