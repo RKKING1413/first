702785e39a8abef72cce3cd9833bd4c1646db29b
RKKING1413 <81173678+RKKING1413@users.noreply.github.com>
2021-08-04 23:38:08 +0530

    Update q2.js

--- q2.js
+++ q2.js
@@ -1,7 +1,7 @@
 /* Q.2 > Write a JavaScript program to display the reading status 
 (i.e. display book name, author name and reading status) of the following books.*/
 
-console.log("start.....");              // test to check if js file is linked or not.
+console.log("start.....");              
 var library = [
     {
     author: 'Bill Gates',
@@ -18,11 +18,10 @@ var library = [
     title: 'Mockingjay: The Final Book of The Hunger Games',
     readingStatus: false
     }];
- // first way to do the task is print it in console.
-console.log(library);
 
-// second way is using index of array to access the sepcfic values from array of objects
-//displaying the name of book name
+console.log(library);
+///////////////////////////////////////////////////////////////////
+
 console.log(library[0].title);
 console.log(library[1].title);
 console.log(library[2].title);
@@ -33,11 +32,12 @@ console.log(library[1].author);
 console.log(library[2].author);
 
 //displaying the reading status
-console.log(library[0].author);
-console.log(library[1].author);
-console.log(library[2].author);
+console.log(library[0].readingStatus);
+console.log(library[1].readingStatus);
+console.log(library[2].readingStatus);
 
 //out put frpm method 1 :
+
 // 0: Object { author: "Bill Gates", title: "The Road Ahead", readingStatus: true }
 // 1: Object { author: "Steve Jobs", title: "Walter Isaacson", readingStatus: true }
 // 2: Object { author: "Suzanne Collins", title: "Mockingjay: The Final Book of The Hunger Games", readingStatus: false }
@@ -45,12 +45,13 @@ console.log(library[2].author);
 
 
 // output from method 2 :
-/*The Road Ahead 
+/*
+ The Road Ahead 
  Walter Isaacson 
  Mockingjay: The Final Book of The Hunger Games 
  Bill Gates 
  Steve Jobs 
  Suzanne Collins 
- Bill Gates 
- Steve Jobs 
- Suzanne Collins*/
+ true
+ true
+ false*/
